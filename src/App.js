067184de2933ed4ce18302f6807bde0449e2198e
Niall Byrne <niall@niallbyrne.ca>
2020-03-21 11:20:37 -0400

    WIP: create initial grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tile from './components/tile/tile.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <div className="wrapper">
+        <div className="section one"><Tile></Tile></div>
+        <div className="section two">CloseUp</div>
+        <div className="section three">Top40</div>
+      </div>
     </div>
   );
 }
